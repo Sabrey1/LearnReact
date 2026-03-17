11bbc127255fac2383179904a398cfe4b525606c
Sabrey1 <limsabriey94@gmail.com>
2026-03-17 15:27:43 +0700

    update

---
+++ react-start/src/App.jsx
@@ -0,0 +1,10 @@
+import Demo from "./Basic/demo1.jsx"
+
+function App(){
+  return(
+    <div>
+      <Demo/>
+    </div>
+  )
+}
+export default App
---
+++ react-start/src/Basic/demo1.jsx
@@ -0,0 +1,9 @@
+function Demo1(){
+    return(
+        <div>
+            <p>Hello world</p>
+        </div>
+    )
+}
+
+export default Demo1
